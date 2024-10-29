12e1c09583b7ada08300d6c0ef6b57abb31de2f0
Claude-Lamarre99 <161344701+Claude-Lamarre99@users.noreply.github.com>
2024-10-29 15:50:28 +0000

    i made some changes

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,6 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+studen_pet_count_list = [0,1,0,2,1,1,4,0,0,0,3,2,1,3,0,2,2,4]
+NUM_Of_STUDENTS = len(studen_pet_count_list)
+print(NUM_Of_STUDENTS)
